ceb25f5e8f66ab7400ae5c1ce25c6095c1ca616a
Nati Yesharim <netanel.yesharim@webcollage.com>
2019-01-04 23:23:26 +0200

    add 404 page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import './App.css';
 import EditLayoutPage from "./Pages/EditLayoutPage";
 import MainPage from './Pages/MainPage';
 import Header from './components/Header';
 import CssBaseline from '@material-ui/core/CssBaseline';
-
 
 const App = () => {
   return (
@@ -15,7 +14,8 @@ const App = () => {
       <Router basename={process.env.PUBLIC_URL}>
         <Switch>
           <Route exact path="/" component={MainPage} />
-          <Route path="/:id" component={EditLayoutPage} />
+          <Route exact path="/:id" component={EditLayoutPage} />
+          <Route component={()=>{return <div><h1>404 Page Not Found</h1> <Link to='/'>ET come home :)</Link> </div>}} />
         </Switch>
       </Router>
     </div>
