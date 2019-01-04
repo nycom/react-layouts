cc31811dde94ac0934851431db7adbda2ec24ffa
Nati Yesharim <netanel.yesharim@webcollage.com>
2019-01-04 21:40:50 +0200

    Updates

---
+++ precache-manifest.f2f7bbaa2742c953f8c24a7a4f079418.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1041ec93714a8ecdb3ed",
+    "url": "/react-layouts/static/js/runtime~main.1041ec93.js"
+  },
+  {
+    "revision": "61c13a30dfb409265aa2",
+    "url": "/react-layouts/static/js/main.61c13a30.chunk.js"
+  },
+  {
+    "revision": "c1afffcc0de80febe5ba",
+    "url": "/react-layouts/static/js/1.c1afffcc.chunk.js"
+  },
+  {
+    "revision": "61c13a30dfb409265aa2",
+    "url": "/react-layouts/static/css/main.d5793046.chunk.css"
+  },
+  {
+    "revision": "48127b11a56a95dcae13cb83e7ee8426",
+    "url": "/react-layouts/index.html"
+  }
+];
